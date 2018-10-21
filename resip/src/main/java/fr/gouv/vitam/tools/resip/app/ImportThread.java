b83916c33502e2e39a2a1f7177b0e7434bee3762
Jean-Séverin Lair <jean-severin.lair@culture.gouv.fr>
2018-10-22 01:04:39 +0200

    Clean .gitignore in sedalib and fix bug in Resip ImporThread

--- resip/src/main/java/fr/gouv/vitam/tools/resip/app/ImportThread.java
+++ resip/src/main/java/fr/gouv/vitam/tools/resip/app/ImportThread.java
@@ -127,7 +127,6 @@ public class ImportThread extends SwingWorker<Work, String> {
                         .getIgnorePatternList())
                     di.addIgnorePattern(ip);
                 di.doImport();
-                ((DiskImportContext) work.getCreationContext()).setModelVersion(di.getModelVersion());
                 setWorkFromArchiveTransfer(di.getArchiveTransfer());
                 summary = mi.getSummary() + "\n" + di.getSummary();
             }
@@ -156,7 +155,7 @@ public class ImportThread extends SwingWorker<Work, String> {
             progressTextArea.setText(progressTextArea.getText() + "\n-> " + "les données n'ont pas été modifiées.");
         else if ((work == null) || (work.getDataObjectPackage() == null))
             progressTextArea.setText(progressTextArea.getText() + "\n-> "
-                    + "Erreur durant l'import, les données n'ont pas été modifiées.\n->" + exitException.getMessage());
+                    + "Erreur durant l'import, les données n'ont pas été modifiées."+(exitException!=null?"\n->" + exitException.getMessage():""));
         else {
             work.getCreationContext().setSummary(summary);
             progressTextArea.setText(progressTextArea.getText() + "\n-> " + summary);
