a320c44a0793795f1507fdca0cfc54979ec1def3
Jean-Séverin Lair <jean-severin.lair@culture.gouv.fr>
2019-09-23 12:54:30 +0200

    improve: add advanced search capacity to ManifestWindow (resip)

--- resip/src/main/java/fr/gouv/vitam/tools/resip/frame/ManifestWindow.java
+++ resip/src/main/java/fr/gouv/vitam/tools/resip/frame/ManifestWindow.java
@@ -28,45 +28,41 @@
 package fr.gouv.vitam.tools.resip.frame;
 
 import fr.gouv.vitam.tools.resip.app.ResipGraphicApp;
-import fr.gouv.vitam.tools.resip.data.Work;
 import fr.gouv.vitam.tools.resip.sedaobjecteditor.SEDAObjectEditor;
 import fr.gouv.vitam.tools.resip.utils.ResipException;
-import fr.gouv.vitam.tools.sedalib.core.ArchiveTransfer;
-import fr.gouv.vitam.tools.sedalib.inout.exporter.ArchiveTransferToSIPExporter;
-import fr.gouv.vitam.tools.sedalib.utils.SEDALibException;
-import org.apache.commons.io.FileUtils;
+import org.fife.rsta.ui.search.FindToolBar;
+import org.fife.rsta.ui.search.SearchEvent;
+import org.fife.rsta.ui.search.SearchListener;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rsyntaxtextarea.SyntaxScheme;
 import org.fife.ui.rsyntaxtextarea.Token;
 import org.fife.ui.rtextarea.RTextScrollPane;
+import org.fife.ui.rtextarea.SearchContext;
+import org.fife.ui.rtextarea.SearchEngine;
+import org.fife.ui.rtextarea.SearchResult;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import static fr.gouv.vitam.tools.resip.sedaobjecteditor.SEDAObjectEditor.*;
-import static fr.gouv.vitam.tools.resip.threads.ExportThread.readableFileSize;
-import static fr.gouv.vitam.tools.sedalib.utils.SEDALibProgressLogger.*;
 
 /**
  * The Class BigTextEditDialog.
  * <p>
  * Class for text edition dialog used for long text metadata like Description or TextContent.
  */
-public class ManifestWindow extends JFrame {
+public class ManifestWindow extends JFrame implements SearchListener {
 
     /**
      * The actions components.
      */
     private RSyntaxTextArea xmlTextArea;
+    private JLabel statusLabel;
 
     // Dialog test context
 
@@ -84,7 +80,7 @@ public class ManifestWindow extends JFrame {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, ResipException, InterruptedException {
         ResipGraphicApp rga = new ResipGraphicApp(null);
         Thread.sleep(1000);
-        ManifestWindow mw=new ManifestWindow();
+        ManifestWindow mw = new ManifestWindow();
     }
 
     /**
@@ -94,13 +90,15 @@ public class ManifestWindow extends JFrame {
         GridBagConstraints gbc;
         GridBagLayout gbl;
 
-        setTitle("Visualisation du manifest généré le "+ LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        setTitle("Visualisation du manifest généré le " + LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
 
         setPreferredSize(new Dimension(1024, 600));
+        setMinimumSize(new Dimension(700, 300));
 
-        gbl=new GridBagLayout();
-        gbl.rowWeights = new double[]{1.0,0.0};
-        gbl.columnWeights = new double[]{0.0,1.0,0.0};
+        gbl = new GridBagLayout();
+        gbl.rowWeights = new double[]{1.0, 0.0};
+        gbl.columnWeights = new double[]{1.0, 0,0, 0.0};
+        gbl.columnWidths = new int[]{400, 0, 200};
         Container contentPane = getContentPane();
         contentPane.setLayout(gbl);
 
@@ -120,10 +118,35 @@ public class ManifestWindow extends JFrame {
         gbc.insets = new Insets(0, 0, 0, 0);
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridx = 0;
-        gbc.gridwidth=3;
+        gbc.gridwidth = 3;
         gbc.gridy = 0;
-        RTextScrollPane editScrollPane=new RTextScrollPane(xmlTextArea);
+        RTextScrollPane editScrollPane = new RTextScrollPane(xmlTextArea);
         contentPane.add(editScrollPane, gbc);
+
+        ManifestFindToolBar findToolBar = new ManifestFindToolBar(this);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(0, 0, 0, 0);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        contentPane.add(findToolBar, gbc);
+
+        JSeparator separator=new JSeparator(SwingConstants.VERTICAL);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        contentPane.add(separator, gbc);
+
+
+        statusLabel = new JLabel("Prêt");
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        contentPane.add(statusLabel, gbc);
 
         pack();
         setLocationRelativeTo(ResipGraphicApp.getTheApp().mainWindow);
@@ -135,7 +158,49 @@ public class ManifestWindow extends JFrame {
      *
      * @param text the text
      */
-    public void setText(String text){
+    public void setText(String text) {
         xmlTextArea.setText(text);
+        xmlTextArea.setCaretPosition(0);
     }
+
+    @Override
+    public void searchEvent(SearchEvent e) {
+
+        SearchEvent.Type type = e.getType();
+        SearchContext context = e.getSearchContext();
+        SearchResult result;
+
+        switch (type) {
+            default: // Prevent FindBugs warning later
+            case MARK_ALL:
+                result = SearchEngine.markAll(xmlTextArea, context);
+                break;
+            case FIND:
+                result = SearchEngine.find(xmlTextArea, context);
+                if (!result.wasFound()) {
+                    UIManager.getLookAndFeel().provideErrorFeedback(xmlTextArea);
+                }
+                break;
+        }
+        String text;
+        if (result.wasFound()) {
+            text = "Trouvé; Occurences marquées: " + result.getMarkedCount();
+        } else if (type == SearchEvent.Type.MARK_ALL) {
+            if (result.getMarkedCount() > 0) {
+                text = "Occurences marquées: " + result.getMarkedCount();
+            } else {
+                text = "";
+            }
+        } else {
+            text = "Trouvé";
+        }
+        statusLabel.setText(text);
+
+    }
+
+    @Override
+    public String getSelectedText() {
+        return xmlTextArea.getSelectedText();
+    }
+
 }
