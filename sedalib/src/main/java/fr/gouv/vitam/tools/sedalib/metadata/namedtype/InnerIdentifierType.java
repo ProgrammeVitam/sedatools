3b4e145e0062c208333d93d637b211c527e9823e
jslair <js.lair@m4x.org>
2022-12-20 14:18:02 +0100

    Sedalib: improve DIP import to be used with VITAM DIP-Full
    - Sedalib: import all footer metadatas
    - Sedalib: add two more metadata classes for this footer, one for the Operation metadata, and another for ArchivalAgency and Requester as a ComplexListType with only an identifier inside
    
    Notice that in Resip theese metadata are not used, but they can be seen in XML format in the log during import
    
    - Fix on all a few sonar issues
    
    - Backlog 12/2022 ID2

---
+++ sedalib/src/main/java/fr/gouv/vitam/tools/sedalib/metadata/namedtype/InnerIdentifierType.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright French Prime minister Office/DINSIC/Vitam Program (2015-2019)
+ * <p>
+ * contact.vitam@programmevitam.fr
+ * <p>
+ * This software is developed as a validation helper tool, for constructing Submission Information Packages (archives
+ * sets) in the Vitam program whose purpose is to implement a digital archiving back-office system managing high
+ * volumetry securely and efficiently.
+ * <p>
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA archiveTransfer the following URL "http://www.cecill.info".
+ * <p>
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ * <p>
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ * <p>
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ */
+package fr.gouv.vitam.tools.sedalib.metadata.namedtype;
+
+import fr.gouv.vitam.tools.sedalib.utils.SEDALibException;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * The Class InnerIdentifierType.
+ * <p>
+ * For abstract <name><Identifier>XXX</Identifier></name> formatted SEDA metadata
+ */
+public class InnerIdentifierType extends ComplexListType {
+
+    /**
+     * Init metadata map.
+     */
+    @ComplexListMetadataMap
+    public static final Map<String, ComplexListMetadataKind> metadataMap;
+
+    static {
+        metadataMap = new LinkedHashMap<>();
+        metadataMap.put("Identifier", new ComplexListMetadataKind(StringType.class, false));
+    }
+
+    // Constructors
+
+    /**
+     * Instantiates a new InnerIdentifierType.
+     */
+    public InnerIdentifierType() {
+        super(null);
+    }
+
+    /**
+     * Instantiates a new InnerIdentifierType.
+     *
+     * @param elementName the XML element name
+     */
+    public InnerIdentifierType(String elementName) {
+        super(elementName);
+    }
+
+    /**
+     * Instantiates a new InnerIdentifierType.
+     *
+     * @param elementName the XML element name
+     * @param value       the value
+     */
+    public InnerIdentifierType(String elementName, String value) throws SEDALibException{
+        super(elementName);
+        if ((value!=null) && !value.isEmpty()) addNewMetadata("Identifier",value);
+    }
+}
