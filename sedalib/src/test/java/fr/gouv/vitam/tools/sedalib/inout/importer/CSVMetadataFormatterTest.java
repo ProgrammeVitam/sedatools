1fc98bd560e8b8ff9c0ffa007c96e4c50fdcfa9c
Paris Alexandre <ldr.paris@gmail.com>
2024-08-08 16:40:37 +0200

    upgrade CSV metadata

--- sedalib/src/test/java/fr/gouv/vitam/tools/sedalib/inout/importer/CSVMetadataFormatterTest.java
+++ sedalib/src/test/java/fr/gouv/vitam/tools/sedalib/inout/importer/CSVMetadataFormatterTest.java
@@ -2,11 +2,14 @@ package fr.gouv.vitam.tools.sedalib.inout.importer;
 
 import fr.gouv.vitam.tools.sedalib.utils.ResourceUtils;
 import fr.gouv.vitam.tools.sedalib.utils.SEDALibException;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CSVMetadataFormatterTest {
 
@@ -25,5 +28,46 @@ class CSVMetadataFormatterTest {
         CSVMetadataFormatter csvMetadataFormatter = new CSVMetadataFormatter(row, ResourceUtils.getResourcePath("metadata_OK.csv").getParent());
         Assertions.assertTrue(csvMetadataFormatter.isExtendedFormat());
     }
+
+    @Test
+    void newCSVMetadataFormatter_test_azdaed() throws FileNotFoundException, SEDALibException {
+        String[] row1 = {
+                "File",
+                "Content.DescriptionLevel",
+                "Content.Title",
+                "Content.SigningInformation.SigningRole.0",
+                "Content.SigningInformation.SigningRole.1",
+                "Content.SigningInformation.SigningRole.2",
+                "Content.SigningInformation.DetachedSigningRole",
+                "Content.SigningInformation.SignatureDescription.0.Signer.FullName",
+                "Content.SigningInformation.SignatureDescription.0.Signer.SigningTime",
+                "Content.SigningInformation.TimestampingInformation.TimeStamp",
+                "Content.SigningInformation.AdditionalProof.0.AdditionalProofInformation"
+        };
+        String[] row2 = {
+                "whatever.pdf",
+                "Item",
+                "whatever",
+                "SignedDocument",
+                "Signature",
+                "Timestamp",
+                "AdditionalProof",
+                "Alexandre PARIS",
+                "2024-12-25T12:34:56",
+                "2023-06-22T11:36:49",
+                "AdditionalProofInformation"
+        };
+        CSVMetadataFormatter metadataFormatter = new CSVMetadataFormatter(row1, Paths.get("/baseDir/"));
+        metadataFormatter.isExtendedFormat();
+
+        Assertions.assertEquals("/baseDir/whatever.pdf", metadataFormatter.getGUID(row2));
+        Assertions.assertEquals("/baseDir", metadataFormatter.getParentGUID(row2));
+        Assertions.assertEquals("/baseDir/whatever.pdf", metadataFormatter.getFile(row2));
+        Assertions.assertEquals("", metadataFormatter.getObjectFiles(row2));
+        assertThat(metadataFormatter.doFormatAndExtractContentXML(row2)).isEqualToIgnoringWhitespace(ResourceUtils.getResourceAsString("import/expected_content_01.xml"));
+        Assertions.assertEquals("",
+                metadataFormatter.extractManagementXML());
+    }
+
 
 }
